aed5dc59069887586dde0815d014f49e6d522c41
Gabriel Yuri Ribeiro <gyribeiro@gmail.com>
2021-09-23 20:49:56 -0300

    test deploy dev4

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from kinesis.stack import KinesisStack
 app = core.App()
 data_lake_stack = DataLakeStack(app)
 common_stack = CommonStack(app)
-kinesis_stack = KinesisStack(
-    app, data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket
-)
-dms_stack = DmsStack(
-    app,
-    common_stack=common_stack,
-    data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket,
-)
+# kinesis_stack = KinesisStack(
+#     app, data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket
+# )
+# dms_stack = DmsStack(
+#     app,
+#     common_stack=common_stack,
+#     data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket,
+# )
 app.synth()
