a91bf445c060eea544c1db122bf5df7ca5f7c5ec
Gabriel Yuri Ribeiro <gyribeiro@gmail.com>
2021-09-23 21:03:17 -0300

    test deploy dev sem common stack

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from kinesis.stack import KinesisStack
 
 app = core.App()
 data_lake_stack = DataLakeStack(app)
-common_stack = CommonStack(app)
+# common_stack = CommonStack(app)
 # kinesis_stack = KinesisStack(
 #     app, data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket
 # )
