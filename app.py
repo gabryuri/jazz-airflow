624de3cd16738fc0406f2dc5b792256b9b93f16e
gabryuri <gyribeiro2014@gmail.com>
2022-02-26 17:39:02 -0300

    add ecs

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from aws_cdk import core
 from common_stack import CommonStack
 from data_lake.stack import DataLakeStack
 from dms.stack import DmsStack
-from kinesis.stack import KinesisStack
+from kinesis.stack import KinesisStacks
+from ec2.stack import ECSCluster
 
 app = core.App()
 #data_lake_stack = DataLakeStack(app)
-common_stack = CommonStack(app)
+#common_stack = CommonStack(app)
+ec2_stack = ECSCluster(app)
 #kinesis_stack = KinesisStack(
 #    app, data_lake_raw_bucket=data_lake_stack.data_lake_raw_bucket
 #)
