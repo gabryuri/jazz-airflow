e132a35dd3f12906b5c8aca93d45b6f9b8cf4f16
gabryuri <gyribeiro2014@gmail.com>
2022-03-09 11:01:07 -0300

    change to airflow

--- infrastructure/ecs/stack.py
+++ infrastructure/ecs/stack.py
@@ -53,7 +53,7 @@ class ECSCluster(core.Stack):
 
 
         container = task_definition_airflow.add_container("DefaultContainer",
-            image=ecs.ContainerImage.from_registry("httpd:2.4"),
+            image=ecs.ContainerImage.from_registry("apache/airflow"),
             memory_limit_mib=512
         )
         #
