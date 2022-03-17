b971ba8c5417445f531db3587468322bd42f8d77
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-03-16 22:01:50 -0300

    add ecr

---
+++ infrastructure/ecr/stack.py
@@ -0,0 +1,17 @@
+from aws_cdk import core
+from aws_cdk import (
+    aws_ecr as ecr
+)
+from constructs import Construct
+
+class ECRStack(core.Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, *kwargs)
+
+        repository = ecr.Repository(self, "Repo",
+            image_scan_on_push=True
+        )
+
+        repository.add_lifecycle_rule(tag_prefix_list=["prod"], max_image_count=150)
+        repository.add_lifecycle_rule(max_image_age=Duration.days(30))
