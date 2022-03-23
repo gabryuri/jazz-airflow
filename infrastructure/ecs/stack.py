06902780d49e2523652032a65a65844869f53d1f
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-03-23 09:36:36 -0300

    change mem limit

--- infrastructure/ecs/stack.py
+++ infrastructure/ecs/stack.py
@@ -59,7 +59,7 @@ class ECSCluster(core.Stack):
         container = task_definition_airflow.add_container("DefaultContainer",
             #image=ecs.ContainerImage.from_registry("puckel/docker-airflow"),
             image= ecs.EcrImage(repo, "prod"),
-            memory_limit_mib=512    
+            memory_limit_mib=470    
         )
         
 
