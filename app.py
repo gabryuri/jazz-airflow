44eafd78f4a4684f3b8d833764952e7b1eff1227
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-04-22 11:45:16 -0300

    add infra

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from aws_cdk import core
 
-from common_stack import CommonStack
 from data_lake.stack import DataLakeStack
-from dms.stack import DmsStack
-from kinesis.stack import KinesisStack
 from ecs.stack import ECSCluster
-from ecr.stack import ECRStack
-from rds.stack import RDSStack
-#from ec_two.stack import EC2Stack
 
 app = core.App()
 
@@ -15,7 +9,7 @@ app = core.App()
 #data_lake_stack = DataLakeStack(app)
 # c  ommon_stack = CommonStack(app)
 ecs = ECSCluster(scope=app, id='Jazz-Ecs-RDS-Airflow')
-ecr = ECRStack(scope=app, id='EcrRepository')
+#ecr = ECRStack(scope=app, id='EcrRepository')
 #rds = RDSStack(scope=app, id='RDSStack')
 
 # test
--- dags/dag_test.py
+++ dags/dag_test.py
@@ -3,13 +3,30 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
-def print_hello():
-    return 'Hello world from first Airflow DAG!'
+import json
+#import boto3
+import os
+from awpy import DemoParser
 
-dag = DAG('dag_do_gabrilo', description='Hello World DAG',
+def parse_demo():
+    demofile = 'demo.dem'
+    demo_parser = DemoParser(demofile=demofile, demo_id="test_parse", parse_rate=128)
+
+    body = demo_parser.parse()
+
+    print(body)
+    return True 
+
+lambda_handler('a','b')
+
+dag = DAG('dag_parse', 
+          description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2021, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+
+
+hello_operator = PythonOperator(task_id='parse_task', python_callable=parse_demo, dag=dag)
 
 hello_operator
