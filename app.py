6d2d01bf21bb00ad338d462be89702cbc41f43bf
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-04-24 21:53:50 -0300

    remove s3 and ecs - fix bugs later

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from basestack import BaseStack
 app = core.App()
 base = BaseStack(scope=app)
 rds = RdsStack(scope=app, id='jazz-RDS-stack', basestack=base)
-ecs = ECSCluster(scope=app, id='Jazz-Ecs-RDS-Airflow', basestack=base)
-s3_storage = S3StorageStack(scope=app)
+#ecs = ECSCluster(scope=app, id='Jazz-Ecs-RDS-Airflow', basestack=base)
+#s3_storage = S3StorageStack(scope=app)
 ecr = ECRStack(scope=app, id='EcrRepository')
 
 app.synth() 
