063bde8104f5511240121a8838a144e34b63c91f
gabryuri <gyribeiro2014@gmail.com>
2022-04-26 21:18:39 -0300

    add deployment

--- infrastructure/ecs/stack.py
+++ infrastructure/ecs/stack.py
@@ -71,8 +71,8 @@ class ECSCluster(core.Stack):
         repo = ecr.Repository.from_repository_name(self, "repo", "ecr-airflow")
 
         container = task_definition_airflow.add_container("DefaultContainer",
-            image=ecs.ContainerImage.from_registry("puckel/docker-airflow:1.10.9"),
-            #image= ecs.EcrImage(repo, "prod"),
+            #image=ecs.ContainerImage.from_registry("puckel/docker-airflow:1.10.9"),
+            image= ecs.EcrImage(repo, "prod"),
             memory_limit_mib=1478,
             environment={
                         'AIRFLOW__CORE__SQL_ALCHEMY_CONN':os.environ["AIRFLOW__CORE__SQL_ALCHEMY_CONN"],   
