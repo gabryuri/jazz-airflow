ae2be327a378ccd4af61e3f3f816532f25fb6d47
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-04-28 20:37:55 -0300

    increase instance size

--- infrastructure/ecs/stack.py
+++ infrastructure/ecs/stack.py
@@ -33,7 +33,7 @@ class ECSCluster(core.Stack):
         )
 
         cluster.add_capacity("DefaultAutoScalingGroupCapacity",
-        instance_type=ec2.InstanceType("t2.micro"),
+        instance_type=ec2.InstanceType("t2.small"),
         desired_capacity=1,
         key_name='ec2-key-pair'
         )
