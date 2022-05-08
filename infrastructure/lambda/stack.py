823f3e4699b87d54491d501b9e8d7c0e6f203fe6
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-05-08 11:03:54 -0300

    add infra

---
+++ infrastructure/lambda/stack.py
@@ -0,0 +1,18 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+)
+
+class LambdaStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('dags/lambda_codes'),
+            handler='rounds.handler',
+        )
