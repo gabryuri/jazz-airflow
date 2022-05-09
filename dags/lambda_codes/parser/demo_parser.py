75f2e2187396796c87da6f9ee99e9c3ba0b1fa07
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-05-09 12:18:05 -0300

    working lambda image for parser

--- dags/lambda_codes/parser/demo_parser.py
+++ dags/lambda_codes/parser/demo_parser.py
@@ -1,55 +1,98 @@
 import os 
 import json
-
+import time 
+import sys 
+import subprocess 
 import boto3
 
-from utils.demoparser import DemoParser
 
-
-demos_folder = ''
-processed_folder = ''
+demos_folder = '/tmp'
+processed_folder = '/tmp'
 
 landing_bucket = 'jazz-landing'
 output_bucket = 'jazz-processed'
 
 exec_date = '2022-03-04'
+
 def handler(event, context):
-
-    #ti = kwargs['ti']
-    #exec_date = kwargs['ds']
-
-    #object_list = ti.xcom_pull(task_ids='scan_for_demos')
-
-    s3_client = boto3.client('s3')
 
     s3_processed_objects = []
 
-    object_list = ['pasta1/demo1.dem', 'pasta2/demo2.dem']
+    s3_object = 'pasta2/demo2.dem'
 
-    for s3_object in object_list:
-        print('PROCESSING FILE ', s3_object)
+    print('PROCESSING FILE ', s3_object)
+    s3_client = boto3.client('s3')
+    demo_path = os.path.join(demos_folder, os.path.basename(s3_object))
+    print('demo being saved to: ', demo_path)
+    s3_client.download_file(landing_bucket, s3_object, demo_path)
         
-        demo_path = os.path.join(demos_folder, os.path.basename(s3_object))
-        print('demo being saved to: ', demo_path)
-        s3_client.download_file(landing_bucket, s3_object, demo_path)
-        
-        demo_name = os.path.basename(s3_object.strip('.dem'))
-        print('demo name: ', demo_name)
-        demo_parser = DemoParser(
-            demofile=demo_path,
-            demo_id=demo_name, 
-            outpath=processed_folder,
-            parse_rate=128)
+    demo_name = os.path.basename(s3_object.strip('.dem'))
+    print('demo name: ', demo_name)
+    path = os.path.join(os.path.dirname(__file__), "")
+    print("Running Golang parser from " + path)
+    print("Looking for file at " + demo_path)
+    parser_cmd = [
+        "go",
+        "run",
+        "parse_demo.go",
+        "-demo",
+        demo_path,
+        "-parserate",
+        "128",
+        "-tradetime",
+        "5",
+        "-buystyle",
+        "hltv",
+        "-demoid",
+        demo_name,
+        "-out",
+        processed_folder,
+    ]
+    print('parser command')
+    print(parser_cmd)
+    # if self.dmg_rolled:
+    #     self.parser_cmd.append("--dmgrolled")
+    # if self.parse_frames:
+    #     self.parser_cmd.append("--parseframes")
+    # if self.json_indentation:
+    #     self.parser_cmd.append("--jsonindentation")
 
-        demo_parser.parse()
+    #self.logger.info(self.parser_cmd)
+    #custom_cmd = ['ls']
+    proc = subprocess.Popen(
+        parser_cmd,
+        #custom_cmd,            #
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT#,
+        #cwd=path
+    )
 
-        local_json_path = os.path.join(processed_folder, demo_name)+".json"
-        s3_object_name = os.path.join(exec_date, demo_name)+".json"
+    process_output, second_output =  proc.communicate()
+    print('process_output')
+    print(str(process_output, 'UTF-8'))
+    print('second_output')
+    print(second_output)
 
-        s3_client.upload_file(local_json_path, output_bucket, s3_object_name)
-        s3_processed_objects.append(local_json_path)
 
-        #todo limpar os dados locais
-    return s3_processed_objects
+
+
+    if os.path.isfile(os.path.join(processed_folder, (demo_name+'.json'))):
+        print("Wrote demo parse output to " + processed_folder + "/" +(demo_name+'.json'))
+
+    time.sleep(10)
+    local_json_path = (os.path.join(processed_folder, demo_name)+".json")
+    print('local_json_path:' , local_json_path)
+    s3_object_name = os.path.join(exec_date, demo_name)+".json"
+
+    
+    with open(local_json_path, 'rb') as f:
+        result = s3_client.upload_fileobj(f, output_bucket, s3_object_name)
+
+    print(result)
+
+    #result = s3_client.upload_file(local_json_path, output_bucket, s3_object_name)
+    s3_processed_objects.append(local_json_path)
+
+    return result
 
 handler('a','s')
