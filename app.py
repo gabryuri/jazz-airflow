2a8fe90d253652511f394517ac4a20981ad8dabd
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-05-09 21:17:31 -0300

    fix

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ rds = RdsStack(scope=app, id='jazz-RDS-stack', basestack=base)
 ecs = ECSCluster(scope=app, id='Jazz-Ecs-Airflow', basestack=base)
 s3_storage = S3StorageStack(scope=app)
 ecr = ECRStack(scope=app, id='Jazz-Ecr-Repository')
-#lambdas = LambdaStack(scope=app, id='Jazz-Lambda-Functions')
+lambdas = LambdaStack(scope=app, id='Jazz-Lambda-Functions')
 app.synth() 
 
