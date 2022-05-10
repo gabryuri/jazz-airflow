0ce7c8383bc573e01aae5d60b8aac5718745f7c5
Gabriel Yuri <gyribeiro2014@gmail.com>
2022-05-09 21:11:34 -0300

    fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from basestack import BaseStack
 app = core.App()
 
 base = BaseStack(scope=app)
-rds = RdsStack(scope=app, id='jazz-RDS-Databases', basestack=base)
+rds = RdsStack(scope=app, id='jazz-RDS-stack', basestack=base)
 ecs = ECSCluster(scope=app, id='Jazz-Ecs-Airflow', basestack=base)
 s3_storage = S3StorageStack(scope=app)
 ecr = ECRStack(scope=app, id='Jazz-Ecr-Repository')
